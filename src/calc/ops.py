c63e0323083ac44bb97ccc1edb92ca69f0af43cc
Cyber Range Student <student@kali.example.com>
2025-11-12 19:14:27 +0000

    Fix flake8 issues: E302/E231/W292 and run black/isort

--- src/calc/ops.py
+++ src/calc/ops.py
@@ -1,13 +1,26 @@
-def add(a: float, b: float) -> float:
-    return a + b
+import math
+import pytest
+from calc import add, sub, mul, div
 
-def sub(a: float, b: float) -> float:
-    return a - b
 
-def mul(a: float, b: float) -> float:
-    return a * b
+def test_add():
+    assert add(2, 3) == 5
 
-def div(a: float, b: float) -> float:
-    if b == 0:
-        raise ZeroDivisionError("b must not be zero")
-    return a / b
+
+def test_sub():
+    assert sub(5, 3) == 2
+
+
+def test_mul():
+    assert mul(4, 3) == 12
+
+
+def test_div():
+    assert math.isclose(div(9, 4), 2.25)
+
+
+@pytest.mark.parametrize("a,b", [(1, 0), (-3, 0)])
+def test_div_zero(a, b):
+    with pytest.raises(ZeroDivisionError):
+        div(a, b)eroDivisionError):
+        div(a, b)
